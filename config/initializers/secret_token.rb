6247655c7f771cdaacfceb67ba180400e1eab7bd
Dosbo <ddosborne@gmail.com>
2014-07-28 17:01:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '0dd7ac6e1594ada6688fd1c4c549cb7b848ae1c4e3698b29221b1a34f5fed6424981f0df6fc2d7e6cb66bda67d99c9874b6b4c4549d10527f6116361e048b488'
